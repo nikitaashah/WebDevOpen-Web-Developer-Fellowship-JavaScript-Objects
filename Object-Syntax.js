af524649e1c79ff947b17d043bfc1af67b60cd5b
Nikita Shah <nikita12.shah@gmail.com>
2024-12-03 14:38:33 +0530

    Adding syntax example for objects in JavaScript.

---
+++ Object-Syntax.js
@@ -0,0 +1,8 @@
+// An object with a mix of data types in JavaScript
+
+let actionFigure = {
+    name : "Super Awesome Guy",
+    heightInCentimeters : 10,
+    accessories : ["cape", "disguise", "laser ring"],
+    limitedEdition : true,
+};
