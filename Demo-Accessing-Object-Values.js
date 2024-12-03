6249f00f804dfa3b232bfec3d4e205e542a7c5a2
Nikita Shah <nikita12.shah@gmail.com>
2024-12-03 19:25:47 +0530

    Adding demo for accessing object values in JavaScript.

---
+++ Demo-Accessing-Object-Values.js
@@ -0,0 +1,11 @@
+// Demo: Accessing Values Inside An Object
+
+let myFavourites = {
+    movie : "Shrek",
+    song : "Bohemian Rhapsody",
+    food : "Lasagna",
+};
+
+console.log(myFavourites.movie);    // 'Shrek'
+console.log(myFavourites.song);     // 'Bohemian Rhapsody'
+console.log(myFavourites.food);     // 'Lasagna'
