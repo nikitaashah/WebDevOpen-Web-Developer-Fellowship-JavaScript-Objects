ee5152904e78622867d62dd5d799403939c1f6f6
Nikita Shah <nikita12.shah@gmail.com>
2024-12-03 14:59:02 +0530

    Adding example for dot notation usage for objects in JavaScript.

---
+++ Dot-Notation-Example.js
@@ -0,0 +1,9 @@
+let actionFigure = {
+    name : "Super Awesome Guy",
+    heightInCentimeters: 10,
+    accessories: ["cape", "disguise", "laser ring"],
+    limitedEdition: true,
+};
+
+actionFigure.name;  // 'Super Awesome Guy'
+actionFigure.limitedEdition;    // true
