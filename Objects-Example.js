8d09c9be8cbfe4a600064525cbefb99c5d4a60ff
Nikita Shah <nikita12.shah@gmail.com>
2024-12-03 14:25:14 +0530

    Adding an example for objects in JavaScript.

---
+++ Objects-Example.js
@@ -0,0 +1,10 @@
+// An example of an object in JavaScript
+
+let gifts = {
+    tommy : "stuffed giraffe",
+    lisa : "coloring book",
+    roberto : "baseball cap",
+    beth: "suit of armor",
+};
+
+gifts["tommy"];     // 'stuffed giraffe'
