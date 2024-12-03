8255d91e63e36a80d0d07731db959bf8db63f612
Nikita Shah <nikita12.shah@gmail.com>
2024-12-03 20:58:58 +0530

    Example for adding key value pairs to objects in JavaScript.

---
+++ Adding-Key-Value-Pairs.js
@@ -0,0 +1,10 @@
+// adding a new key/value pair
+let myFamily = {
+    mom: "Cynthia",
+    dad: "Paul",
+  };
+  
+  myFamily.sister = "Lucinda"; // dot notation
+  myFamily["brother in law"] = "Merle"; // bracket notation
+
+  console.log(myFamily);
