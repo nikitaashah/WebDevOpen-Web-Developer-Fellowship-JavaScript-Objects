51511a996717507458ceea3ffd4994cd95137ac5
Nikita Shah <nikita12.shah@gmail.com>
2024-12-03 21:17:31 +0530

    Adding solution for fruit basket drill.

---
+++ Drill-Fruit-Basket.js
@@ -0,0 +1,31 @@
+// Object Notation Practice
+// Drill - Fruit Basket
+
+/*
+You're putting together a colorful fruit basket for a friend, and you're using an object to plan what fruit you'll put in. 
+Use the syntax for adding values to add the following fruits to the fruitBasket object in the code below. 
+The fruit name is the new key, and the color is the new value.
+
+banana: "yellow";
+avocado: "green";
+strawberry: "red";
+
+*/
+
+// Adding key/value pairs to objects
+// don't modify the object here
+
+let fruitBasket = {
+    apple: "red",
+    blackberry: "black",
+    orange: "orange",
+};
+  
+// Add the other fruits and colors using dot notation below
+// NOTE: Remember that they are case-senstive
+
+fruitBasket['banana'] = "yellow";
+fruitBasket['avocado'] = "green";
+fruitBasket['strawberry'] = "red";
+
+console.log(fruitBasket);
